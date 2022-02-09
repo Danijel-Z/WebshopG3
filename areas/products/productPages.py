f52109d7858fab27f690d66b850d8bf0e514b547
Danijel-Z <danijelswda@hotmail.com>
2022-02-10 00:15:59 +0100

    newsletter 1.0

--- areas/products/productPages.py
+++ areas/products/productPages.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect,url_for
 from .services import getCategory, getTrendingCategories, getProduct, getTrendingProducts
-
+from models import db, Newsletter
+from .forms import NewsLetterForm
 
 
 productBluePrint = Blueprint('product', __name__)
@@ -8,14 +9,24 @@ productBluePrint = Blueprint('product', __name__)
 
 
 
-@productBluePrint.route('/')
+@productBluePrint.route('/', methods = ["POST", "GET"])
 def index() -> str:
+    form = NewsLetterForm()
+
+    if form.validate_on_submit():
+        newSubscriber = Newsletter()
+        newSubscriber.email = form.email.data
+        db.session.add(newSubscriber)
+        db.session.commit()
+
+        flash("Tack! Du är nu prenumenerat!")
+        return redirect(url_for("product.index", inputedEmail = form.email.data, form=form))
+    
     trendingCategories = []
     trendingCategories = getTrendingCategories()
     trendingProducts = getTrendingProducts()
     return render_template('products/index.html',trendingCategories=trendingCategories,
-        products=trendingProducts
-    )
+        products=trendingProducts, form= form, inputedEmail = form.email.data)
 
 
 @productBluePrint.route('/category/<id>')
