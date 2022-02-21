23e1fc35f4ac7d22de93800662536929c1ba7805
Bankopm <30323460+Bankopm@users.noreply.github.com>
2022-02-21 15:13:11 +0100

    Baris final

--- areas/products/productPages.py
+++ areas/products/productPages.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, flash, redirect,url_for, request
-from .services import getCategory, getTrendingCategories, getProduct, getTrendingProducts
-from models import db, Newsletter
+from flask import Blueprint, render_template, flash, redirect,url_for, request, session
+from flask_login import current_user
+from .services import getCategory, getTrendingCategories, getProduct, getTrendingProducts, merge_dicts, checkIfNewsletterSubscribed, cart_grandtotal
+from models import db, Newsletter, Product
 from .forms import NewsLetterForm
 
 
@@ -13,7 +14,7 @@ productBluePrint = Blueprint('product', __name__)
 def index() -> str:
     ##### Start Danijels kod #####
     form = NewsLetterForm()
-    
+    NewsletterPage = True
     if form.validate_on_submit():
         newSubscriber = Newsletter()
         newSubscriber.email = form.email.data
@@ -24,6 +25,14 @@ def index() -> str:
         return redirect(url_for("product.index"))
     
     inputedEmail = request.form.get("email", "")
+    if current_user.is_authenticated:
+
+        subscribed = checkIfNewsletterSubscribed(current_user.email)
+
+    else:
+
+        subscribed = False
+    
     ##### Slut av Danijels kod #####
 
     trendingCategories = []
@@ -31,13 +40,14 @@ def index() -> str:
     trendingProducts = getTrendingProducts()
 
     return render_template('products/index.html', trendingCategories=trendingCategories,
-        products=trendingProducts, form = form, inputedEmail = inputedEmail)
+        products=trendingProducts, form = form, inputedEmail = inputedEmail, NewsletterPage = NewsletterPage, subscribed=subscribed)
 
 
 @productBluePrint.route('/category/<id>')
 def category(id) -> str:
     category = getCategory(id)
-    return render_template('products/category.html',category=category)
+    
+    return render_template('products/category.html',category=category )
 
 @productBluePrint.route('/product/<id>')
 def product(id) -> str:
@@ -45,5 +55,101 @@ def product(id) -> str:
     return render_template('products/product.html',product=product)
 
 
+@productBluePrint.route('/viewcart', methods=["GET"])
+def viewcart():
+    if "shoppingcart" not in session:
+        return redirect(request.referrer)
+    cart = session["shoppingcart"]
+    grandtotal = cart_grandtotal(cart)
+    return render_template('products/viewcart.html', cart = cart, grandtotal = grandtotal )
+
+@productBluePrint.route('/checkout', methods=["POST", "GET"])
+def checkout():
+    if "shoppingcart" not in session:
+        return redirect(request.referrer)
+    cart = session["shoppingcart"]
+    return render_template('products/checkout.html', cart = cart )
+
+@productBluePrint.route('/add_cart', methods=["POST", "GET"])
+def add_cart():
+    try:
+        product_id = request.args.get("id")
+        product = Product.query.get(product_id)
+        dict_items = {product_id:{"name":product.ProductName, "price": product.UnitPrice, 
+        "image":"https://picsum.photos/300/300?" + str(product.ProductID),"quantity":1,"discount":product.Discontinued}}
+        
+        if "shoppingcart" in session:
+            session["shoppingcart"] = merge_dicts(session["shoppingcart"],dict_items)
+            return redirect(request.referrer)
+        else:
+            session["shoppingcart"] = dict_items 
+            return redirect(request.referrer)
+    except Exception as e:
+        print(e)
+    finally:
+        return redirect(request.referrer)
+
+@productBluePrint.route('/updatecart/<int:code>', methods=["POST"])
+def update_cart(code): 
+    if request.method =="POST":
+        quantity= request.form.get("quantity")
+        try: 
+            session.modified = True
+            for key, item in session["shoppingcart"].items():
+                if int(key) == code:
+                    item["quantity"] = int(quantity)
+                    return redirect(url_for("product.viewcart"))
+        except Exception as e:
+            print(e)
+            return redirect(url_for("product.viewcart"))
+
+@productBluePrint.route('/updatecart/<int:id>', methods=["POST","GET"])      
+def delete_item(id):
+    try:
+        session.modified = True
+        for key , item in session['shoppingcart'].items():
+            if int(key) == id:
+                session['shoppingcart'].pop(key, None)
+                return redirect(request.referrer)
+    except Exception as e:
+        print(e)
+        return redirect(url_for('product.viewcart'))
 
 
+@productBluePrint.route('/wishlist', methods=["GET"])
+def wishlist():
+    if "wishlist" not in session:
+        return redirect(request.referrer)
+    wishcart = session["wishlist"]
+    return render_template('products/wishlist.html', wishcart = wishcart )
+
+@productBluePrint.route('/addwishlist', methods=["POST", "GET"])
+def addwishlist():
+    try:
+        product_id = request.args.get("id")
+        product = Product.query.get(product_id)
+        dict_items = {product_id:{"name":product.ProductName, "price": product.UnitPrice, 
+        "image":"https://picsum.photos/300/300?" + str(product.ProductID),"quantity":1,"discount":product.Discontinued}}
+        
+        if "wishlist" in session:
+            session["wishlist"] = merge_dicts(session["wishlist"],dict_items)
+            return redirect(request.referrer)
+        else:
+            session["wishlist"] = dict_items 
+            return redirect(request.referrer)
+    except Exception as e:
+        print(e)
+    finally:
+        return redirect(request.referrer)
+
+@productBluePrint.route('/delete_wish/<int:id>', methods=["POST","GET"])      
+def delete_wish_item(id):
+    try:
+        session.modified = True
+        for key , item in session['wishlist'].items():
+            if int(key) == id:
+                session['wishlist'].pop(key, None)
+                return redirect(request.referrer)
+    except Exception as e:
+        print(e)
+        return redirect(request.referrer)
--- areas/products/services.py
+++ areas/products/services.py
@@ -1,4 +1,5 @@
-from models import Category, Product
+
+from models import Category, Product, Newsletter
 
 def getTrendingCategories():
     return Category.query.order_by(Category.CategoryID.desc()).paginate(1,4,False).items
@@ -11,3 +12,23 @@ def getProduct(id):
 
 def getTrendingProducts():
     return Product.query.order_by(Product.ProductID.desc()).paginate(1,8,False).items
+
+def merge_dicts(dict1,dict2):
+    if isinstance(dict1, dict) and isinstance(dict2, dict):
+            key = list(dict2)[0]
+            if key in dict1.keys():
+                dict1[key]["quantity"] += 1
+                return dict1
+            else:
+                return dict(list(dict1.items()) + list(dict2.items()))
+
+def checkIfNewsletterSubscribed(email:str)-> bool:
+    subscribed = Newsletter.query.filter(Newsletter.email == email).first()
+    return True if subscribed else False
+
+def cart_grandtotal(dict:dict):
+    grandtotal = 0
+    for key, product in dict.items():
+        subtotal = float(product["price"]) * int(product["quantity"])
+        grandtotal = grandtotal + subtotal
+    return grandtotal
